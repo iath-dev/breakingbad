f91a7f62c0b01841af8ac695ef39d8c6d7b801b9
iath-dev <developer.iath@gmail.com>
2020-02-04 09:55:24 -0500

    106. Obteniendo una frase desde la API

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Button, Container } from './components';
 
 function App() {
+  
+  const [phrase, setPhrase] = React.useState({});
 
-  const consultApi = () => {
-    console.log('Consultando....');
+  const consultApi = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const result = await api.json()
+    setPhrase(result[0]);
   }
 
   return (
